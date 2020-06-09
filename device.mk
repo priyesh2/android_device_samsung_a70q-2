cc635c8e5b619fda6cb13ad9274736bacc55755b
Michael Benedict <michaelbt@live.com>
2020-06-10 03:38:30 +1000

    a70q: build samsung doze
    Signed-off-by: Michael Benedict <michaelbt@live.com>

--- device.mk
+++ device.mk
@@ -90,6 +90,10 @@ PRODUCT_PACKAGES += \
     fastbootd \
     init.recovery.qcom.rc
 
+# SamsungDoze
+PRODUCT_PACKAGES += \
+    SamsungDoze
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl.a70q
