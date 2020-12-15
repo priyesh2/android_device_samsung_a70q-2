7473728b09e5f32fc369e84e989f4be973f7eaca
Haky86 <hakamelassouad@gmail.com>
2020-12-15 02:19:03 +0100

    a70q: inherit product_launched_with_p
    * Our device has been launched with P.

--- lineage_a70q.mk
+++ lineage_a70q.mk
@@ -7,6 +7,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit from a70q device
 $(call inherit-product, device/samsung/a70q/device.mk)
@@ -32,3 +33,13 @@ PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
+
+# Use the latest approved GMS identifiers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="a70q" \
+    TARGET_BOOTLOADER_BOARD_NAME=sm6150 \
+    TARGET_DEVICE="a70q" \
+    PRODUCT_NAME=a70q \
+    PRIVATE_BUILD_DESC="a70qxx-user 9 PPR1.180610.011 A705FNXXU5ASL4 release-keys" \
+
+BUILD_FINGERPRINT := "samsung/a70qxx/a70q:9/PPR1.180610.011/A705FNXXU5ASL4:user/release-keys"
